0bd3cb97600216b0e0225f204bff7b91f3b3caa6
Ernest Omondi <eomondi@fb.com>
2018-07-11 20:57:38 -0700

    Changed the location of like and comment buttons

---
+++ Instagram/View Controllers/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Instagram
+//
+//  Created by Ernest Omondi on 7/11/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
